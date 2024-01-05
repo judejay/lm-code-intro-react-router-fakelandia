4b1053091832f5b192d5be19335ad1199a67e77a
judejay <judejay26@gmail.com>
2024-01-05 13:19:51 +0000

    Style: navbar

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,8 +1,18 @@
 import Nav from "./Nav";
-
+import "./Header.css";
 const Header = () => (
   <header>
-    <h1 className="title">Fakelandia Justice Department</h1>
+    <div>
+      <div className="title-container">
+        <p id="top-title">Fakelandia</p>
+      </div>
+      <div>
+        <p className="title">Justice</p>
+      </div>
+      <div>
+        <p className="title">Department</p>
+      </div>
+    </div>
     <Nav />
   </header>
 );
