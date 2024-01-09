4de6b652aa7b62ce3a76e2ca26ed430311c226d2
judejay <judejay26@gmail.com>
2024-01-09 20:52:43 +0000

    style: center table

--- client/src/components/Misdemeanor.tsx
+++ client/src/components/Misdemeanor.tsx
@@ -2,11 +2,10 @@ import { Misdemeanour } from "../types/misdemeanor.types";
 import { useMyContext } from "../hooks/useContext";
 import { useEffect, useState } from "react";
 import Incident from "./Incident";
-
+import "./Misdemeanor.css";
 export interface ResponseData {
   misdemeanours: Misdemeanour[];
 }
-//const url = "https://picsum.photos/100/100";
 
 const Misdemeanor: React.FC = () => {
   const [data, setData] = useState<Misdemeanour[]>([]);
@@ -26,26 +25,28 @@ const Misdemeanor: React.FC = () => {
   }, [fetchData]);
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Citizen ID</th>
-          <th>Date</th>
-          <th>Misdemeanour</th>
-          <th>Punishment</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((incident) => (
-          <Incident
-            key={incident.citizenId}
-            citizenId={incident.citizenId}
-            date={incident.date}
-            misdemeanour={incident.misdemeanour}
-          />
-        ))}
-      </tbody>
-    </table>
+    <div className="table-container">
+      <table>
+        <thead>
+          <tr>
+            <th>Citizen ID</th>
+            <th>Date</th>
+            <th>Misdemeanour</th>
+            <th>Punishment</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((incident) => (
+            <Incident
+              key={incident.citizenId}
+              citizenId={incident.citizenId}
+              date={incident.date}
+              misdemeanour={incident.misdemeanour}
+            />
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 export default Misdemeanor;
